cdf97ff7bb5815bedc7e737b10cacb5b710772b8
Chris Meller <chris@doesnthaveone.com>
2011-10-01 14:57:30 -0400

    Namespace the root PKCS5 class and add factory methods for easily grabbing our two sub-classes.

--- classes/pkcs5.php
+++ classes/pkcs5.php
@@ -1,8 +1,20 @@
 <?php
 
+	namespace PKCS5;
+
 	class PKCS5 {
 		
+		public static function pbkdf2 ( $password, $salt ) {
+			
+			return new PBKDF2( $password, $salt );
+			
+		}
 		
+		public static function pbes2 ( $password, $salt ) {
+			
+			return new PBES2( $password, $salt );
+			
+		}
 		
 	}
 
